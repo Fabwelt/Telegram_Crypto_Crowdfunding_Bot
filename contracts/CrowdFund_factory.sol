f42a27c708af743049baf3a4454ef21d636bda5d
Fabwelt <87412461+Fabwelt@users.noreply.github.com>
2024-08-28 10:46:19 +0200

    Factory update
    Options added soft- and hardcap
    ReentrancyGuard

--- contracts/CrowdFund_factory.sol
+++ contracts/CrowdFund_factory.sol
@@ -6,13 +6,16 @@ import "./Crowdfunding.sol";
 contract CrowdfundingFactory {
     Crowdfunding[] public campaigns;
 
-    event CampaignCreated(address campaignAddress, address creator, uint goal, uint duration);
+    event CampaignCreated(address indexed campaignAddress, address indexed creator, uint softCap, uint hardCap, uint duration);
 
-    function createCampaign(uint _goal, uint _duration) external {
-        Crowdfunding newCampaign = new Crowdfunding(msg.sender, _goal, _duration);
+    function createCampaign(uint _softCap, uint _hardCap, uint _duration) external {
+        require(_softCap > 0, "Soft cap must be greater than 0");
+        require(_hardCap > _softCap, "Hard cap must be greater than soft cap");
+
+        Crowdfunding newCampaign = new Crowdfunding(msg.sender, _softCap, _hardCap, _duration);
         campaigns.push(newCampaign);
 
-        emit CampaignCreated(address(newCampaign), msg.sender, _goal, _duration);
+        emit CampaignCreated(address(newCampaign), msg.sender, _softCap, _hardCap, _duration);
     }
 
     function getCampaigns() external view returns (Crowdfunding[] memory) {
